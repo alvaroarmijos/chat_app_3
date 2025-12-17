b320e51c918d4d2c0eadc670f3d170901cec333d
Alvaro Armijos <alvaro.armijoss@gmail.com>
2025-12-16 20:57:24 -0500

    clase-01

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,46 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(title: const Text('Material App Bar')),
-        body: const Center(child: Text('Hello World')),
+        backgroundColor: Colors.black,
+        body: Stack(
+          children: [
+            Image.asset(
+              'assets/images/background.png',
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Image.asset('assets/icons/icon_app.png'),
+                    Text(
+                      'Connect friends easily & quickly',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 68,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      'Our chat app is the perfect way to stay connected with friends and family.',
+                      style: TextStyle(fontSize: 16, color: Color(0xFFB9C1BE)),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Image.asset('assets/icons/icon_facebook.png'),
+                        Image.asset('assets/icons/icon_google.png'),
+                        Image.asset('assets/icons/icon_apple.png'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
