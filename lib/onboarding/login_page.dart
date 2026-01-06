f42d41d2f76d2fb10c72b4300e27662992928d87
Alvaro Armijos <alvaro.armijoss@gmail.com>
2026-01-05 20:08:30 -0500

    clase 04

--- lib/onboarding/login_page.dart
+++ lib/onboarding/login_page.dart
@@ -7,80 +7,135 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final formKey = GlobalKey<FormState>();
+
     return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              children: [
-                RichText(
-                  text: TextSpan(
-                    text: 'Log in',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                      decoration: TextDecoration.underline,
-                      decorationColor: Color(0xFF58C3B6).withValues(alpha: 0.7),
-                      decorationThickness: 5,
-                    ),
-                    children: [
-                      TextSpan(
-                        text: ' to Chateo',
-                        style: TextStyle(decoration: TextDecoration.none),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(40),
-                  child: Text(
-                    'Welcome back! Sign in using your social account or email to continue us',
-                    style: TextStyle(fontSize: 14, color: Color(0xFF797C7B)),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                SocialMediaBar(appleIconColor: Colors.black),
-                OnboardingDivider(
-                  textColor: Color(0xFF797C7B),
-                  dividerColor: Color(0xFFCDD1D0),
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: TextFormField(
-                    cursorColor: Color(0xFF24786D),
-                    decoration: InputDecoration(
-                      labelText: 'Your email',
-                      labelStyle: TextStyle(color: Color(0xFF24786D)),
-                      border: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey),
-                      ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Stack(
+            children: [
+              Form(
+                key: formKey,
+                child: ListView(
+                  children: [
+                    const SizedBox(height: 20),
+                    RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                        text: 'Log in',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          decoration: TextDecoration.underline,
+                          decorationColor: Color(
+                            0xFF58C3B6,
+                          ).withValues(alpha: 0.7),
+                          decorationThickness: 5,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: ' to Chateo',
+                            style: TextStyle(decoration: TextDecoration.none),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: TextFormField(
-                    cursorColor: Color(0xFF24786D),
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      labelStyle: TextStyle(color: Color(0xFF24786D)),
-                      border: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey),
-                      ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey),
+                    const SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.all(40),
+                      child: Text(
+                        'Welcome back! Sign in using your social account or email to continue us',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Color(0xFF797C7B),
+                        ),
+                        textAlign: TextAlign.center,
                       ),
                     ),
-                  ),
+                    const SizedBox(height: 20),
+                    SocialMediaBar(appleIconColor: Colors.black),
+                    const SizedBox(height: 20),
+                    OnboardingDivider(
+                      textColor: Color(0xFF797C7B),
+                      dividerColor: Color(0xFFCDD1D0),
+                    ),
+                    TextFormField(
+                      cursorColor: Color(0xFF24786D),
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Your email',
+                        labelStyle: TextStyle(
+                          color: Color(0xFF24786D),
+                          fontWeight: FontWeight.normal,
+                          fontSize: 14,
+                        ),
+                        border: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFFD6E4E0)),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFFD6E4E0)),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFFD6E4E0)),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email';
+                        }
+
+                        final emailRegex = RegExp(
+                          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+                        );
+
+                        if (!emailRegex.hasMatch(value)) {
+                          return 'Please enter a valid email address';
+                        }
+
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      cursorColor: Color(0xFF24786D),
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        labelStyle: TextStyle(
+                          color: Color(0xFF24786D),
+                          fontWeight: FontWeight.normal,
+                          fontSize: 14,
+                        ),
+                        border: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFFD6E4E0)),
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFFD6E4E0)),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFFD6E4E0)),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        }
+                        if (value.length < 8) {
+                          return 'Password must be at least 8 characters long';
+                        }
+                        return null;
+                      },
+                    ),
+                  ],
                 ),
-                SizedBox(
+              ),
+              Align(
+                alignment: AlignmentGeometry.bottomCenter,
+                child: Container(
+                  color: Colors.white,
                   width: double.infinity,
                   child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
@@ -90,12 +145,20 @@ class LoginPage extends StatelessWidget {
                         borderRadius: BorderRadius.circular(16),
                       ),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      final isValid = formKey.currentState?.validate();
+
+                      if (isValid ?? false) {
+                        print('Formulario válido');
+                      } else {
+                        print('Formulario inválido');
+                      }
+                    },
                     child: Text('Sign up withn mail'),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
